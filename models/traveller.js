3aa873fae2b3a3c4b6d7d89091d332a9d2beb2e3
calumgould <calumgould26@gmail.com>
2020-02-19 17:06:52 +0000

    initial commit & MVP

---
+++ models/traveller.js
@@ -0,0 +1,45 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) =>  {
+    return journey.startLocation;
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal += journey.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  let transports = this.journeys.map((journey) => {
+    return journey.transport;
+  });
+  return transports.filter((transport, index) => {
+    return transports.indexOf(transport) === index;
+  });
+};
+
+
+module.exports = Traveller;
